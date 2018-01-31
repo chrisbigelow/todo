520ae3345510371b389fba6cf48e1b40a806f8e3
Christopher Bigelow <chrisbigelow25@gmail.com>
2018-01-31 15:18:09 -0800

    initial commit

---
+++ frontend/todo_app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const root = document.getElementById('content');
+  ReactDOM.render(<h1>Todos App</h1>, root);
+});
