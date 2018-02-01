8b72c32fa94735adcb4f0b8e28042ee89489e737
Christopher Bigelow <chrisbigelow25@gmail.com>
2018-01-31 22:38:06 -0800

    add store and reducer

--- frontend/todo_app.jsx
+++ frontend/todo_app.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
+  
+
+  const preloadedState = {};
+
+  const store = configureStore(preloadedState);
+
+  window.store = store;
+
   const root = document.getElementById('content');
-  ReactDOM.render(<h1>Todos App</h1>, root);
+  // ReactDOM.render(<Root store={store}></Root>, root);
 });
